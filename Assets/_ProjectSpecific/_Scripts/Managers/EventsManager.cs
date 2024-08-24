d77f61a6d19d20db0264ca9f7c4cf6955ced691b
gabancianodev <dev.gabanciano@gmail.com>
2024-08-24 22:59:04 +0800

    Added Events

--- Assets/_ProjectSpecific/_Scripts/Managers/EventsManager.cs
+++ Assets/_ProjectSpecific/_Scripts/Managers/EventsManager.cs
@@ -1,18 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class EventsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static EventsManager Instance { get; private set; }
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    #region EVENTS DECLARATION
+    public event Action OnPlayerJump;
+
+    public event Action OnPlayerLanded;
+
+    public event Action OnPlayerHit;
+
+    public event Action OnPlayerDead;
+
+    #endregion ---
+    #region EVENTS INVOCATION
+
+    public void InvokeOnPlayerJump()
     {
-        
+        OnPlayerJump.Invoke();
     }
+
+    public void InvokeOnPlayerLanded()
+    {
+        OnPlayerJump.Invoke();
+    }
+    
+    public void InvokeOnPlayerHit()
+    {
+        OnPlayerHit.Invoke();
+    }
+
+    public void InvokeOnPlayerDead(){
+        OnPlayerDead.Invoke();
+    }
+
+    #endregion ---
 }
